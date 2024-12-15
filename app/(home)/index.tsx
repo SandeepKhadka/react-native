f05fabf9ed5273f6c526faf495825d3e072ac87a
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-12-15 15:00:01 +0545

    native app

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,11 +1,12 @@
 import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 
 export default function HomeIndex() {
   return (
     <View>
       <Text>Welcome to the Home Page!</Text>
-      <Link href="/(home)/details">Go to Details</Link>
+      {/* <Link href="/(home)/details">Go to Details</Link> */}
+      router.push("./details", {relativeToDirectory: true})
     </View>
   );
 }
