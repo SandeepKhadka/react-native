6ee505708f596f6d5c9815d120b088453f1bc7f9
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-12-16 14:48:54 +0545

    Creating nav bar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { View } from "react-native";
+
+export default function Navbar() {
+    return (
+        <View>
+            <Link href="/home">Home</Link>
+            <Link href="/matches"></Link>
+            <Link href="/modes">Modes</Link>
+            <Link href="/profile">Profile</Link>
+        </View>
+    )
+}
